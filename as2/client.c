386c336b8b4ae353254eee888cb23e927d2024a6
pyeojim <131635555+pyeojim@users.noreply.github.com>
2025-06-04 20:52:27 +0900

    Update client.c

--- as2/client.c
+++ as2/client.c
@@ -244,9 +244,6 @@ void handle_server_message(char *buffer) {
                     printf("[Client] Sending Move: (%d,%d)->(%d,%d)\n", sR, sC, tR, tC);
                 }
 
-                printf("[Client] Board after move generation (예상):\n");
-                printBoard(&game_board);
-
                 send_move_message(&move);  // 내부에서 "JSON + '\n'" 전송됨
                 client_state = CLIENT_WAITING;
             }
@@ -259,8 +256,7 @@ void handle_server_message(char *buffer) {
             char nextPlayer[64];
             if (parseMoveResultMessage(json_obj, &updated_board, nextPlayer)) {
                 memcpy(&game_board, &updated_board, sizeof(GameBoard)); // 로컬 보드 동기화
-                printf("[Client] Received move_ok. Board updated:\n");
-                printBoard(&game_board);
+                printf("[Client] Received move_ok.\n");
                 
                 if (strcmp(nextPlayer, my_username) == 0) {
                     printf("[Client] It's your turn next.\n");
@@ -278,8 +274,7 @@ void handle_server_message(char *buffer) {
             if (parseMoveResultMessage(json_obj, &updated_board, nextPlayer)) {
                 memcpy(&game_board, &updated_board, sizeof(GameBoard));
             }
-            printf("[Client] Opponent passed. Board now:\n");
-            printBoard(&game_board);
+            printf("[Client] Opponent passed.\n");
             break;
         }
         
